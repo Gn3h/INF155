20eec2b2d03af2f12cc7dc3ec44a9e682fb7c94b
Harry Heng <harryheng2002@gmail.com>
2026-04-15 17:05:24 -0400

    projet _test

---
+++ build/CMakeFiles/my_program.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_program.dir/link.d"
+  "CMakeFiles/my_program.dir/src/main.c.o"
+  "CMakeFiles/my_program.dir/src/main.c.o.d"
+  "CMakeFiles/my_program.dir/src/math_utils.c.o"
+  "CMakeFiles/my_program.dir/src/math_utils.c.o.d"
+  "my_program"
+  "my_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
